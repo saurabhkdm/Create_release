27361341ad724d9fe35cd4ea61cbd23147dede55
saurabhkdm <66935356+saurabhkdm@users.noreply.github.com>
2020-06-25 16:29:12 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -18,7 +18,7 @@ then
     DATA="${DATA} $(printf '"body":"Automated release based on keyword: %s",' "$*")"
     DATA="${DATA} $(printf '"draft":false, "prerelease":false}')"
 
-    URL="https://api.github.com/repos/${GITHUB_REPOSITORY}/releases?access_token=${GITHUB_TOKEN}"
+    URL="https://api.github.com/repos/saurabhkdm/Create_release/releases?access_token=$1"
 
     if [[ "${LOCAL_TEST}" == *"true"* ]];
     then
