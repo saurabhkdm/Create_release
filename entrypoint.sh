637b5ee9b5d585d5bc9fb63007e757128c34a07f
saurabhkdm <66935356+saurabhkdm@users.noreply.github.com>
2020-06-25 16:34:22 +0530

    Fixed

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,28 +6,13 @@ EVENT_PATH=$GITHUB_EVENT_PATH
 env
 jq . < $EVENT_PATH
 
-# if keyword is found
-if jq '.commits[].message, .head_commit.message' < $EVENT_PATH | grep -i -q "$*";
-then
-    # do something
-    VERSION=$(date +%F.%s)
+VERSION=$(date +%F.%s)
 
-    DATA="$(printf '{"tag_name":"v%s",' $VERSION)"
-    DATA="${DATA} $(printf '"target_commitish":"master",')"
-    DATA="${DATA} $(printf '"name":"v%s",' $VERSION)"
-    DATA="${DATA} $(printf '"body":"Automated release based on keyword: %s",' "$*")"
-    DATA="${DATA} $(printf '"draft":false, "prerelease":false}')"
+DATA="$(printf '{"tag_name":"v%s",' $VERSION)"
+DATA="${DATA} $(printf '"target_commitish":"master",')"
+DATA="${DATA} $(printf '"name":"v%s",' $VERSION)"
+DATA="${DATA} $(printf '"body":"Automated release based on keyword: %s",' "$*")"
+DATA="${DATA} $(printf '"draft":false, "prerelease":false}')"
 
-    URL="https://api.github.com/repos/saurabhkdm/Create_release/releases?access_token=$1"
-
-    if [[ "${LOCAL_TEST}" == *"true"* ]];
-    then
-        echo "## [TESTING] Keyword was found but no release was created."
-    else
-        echo $DATA | http POST $URL | jq .
-    fi
-# otherwise
-else
-    # exit gracefully
-    echo "Nothing to process."
-fi
+URL="https://api.github.com/repos/saurabhkdm/Create_release/releases?access_token=$1"
+echo $DATA | http POST $URL | jq .
